487aeb9ba99db24104a55ad9b64d97bede9f9630
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-12-13 19:07:36 +0100

    add a readme

--- PokerTest.php
+++ PokerTest.php
@@ -11,5 +11,6 @@ class PokerTest extends TestCase
     {
         $poker = new Poker();
         $this->assertEquals('nothing',$poker->retrieveCombinaison([['A','H'],['2','C'],['4','S'],['8','D'],['10','S']]));
+        $this->assertEquals('nothing',$poker->retrieveCombinaison([['A','H'],['2','C'],['4','S'],['8','D'],['10','S']]));
     }
 }
---
+++ README.md
@@ -0,0 +1,13 @@
+###Poker hands   
+
+Retourne le nom de la combinaison en fonction de la main reçue   
+
+
+| Français | Anglais     |
+|----------|-------------|
+| Coeur    | Heart (H)   |
+| Carreau  | Diamond (D) |
+| Pique    | Spade (S)   |
+| Trèfle   | Club (C)    |
+
+![](poker_hands.png)
