4102be70000802f9192fbfcc8f5d1f0b5bb59afb
Olivier Chatelin <olivier.chatelin@gmail.com>
2021-12-13 21:49:31 +0100

    done

--- Poker.php
+++ Poker.php
@@ -4,8 +4,102 @@ namespace app;
 
 class Poker
 {
-    public function retrieveCombinaison(array $hand): string
+    public const ORDER_VALUES = [
+        'A' => 1,
+        '2' => 2,
+        '3' => 3,
+        '4' => 4,
+        '5' => 5,
+        '6' => 6,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9,
+        '10' => 10,
+        'J' => 11,
+        'Q' => 12,
+        'K' => 13
+    ];
+    public function retrieveCombinaison(array $cards): string
     {
-        return 'nothing';
+    $combinaisons = [
+      'One pair' => 0,
+      'Two pairs' => 0,
+      'Three of a kind' => 0,
+      'Straight' => 0,
+        'Flush' => 0,
+        'Full house' => 0,
+        'Four of a kind' => 0,
+        'Straight flush' => 0,
+        'Royal flush' => 0,
+    ];
+
+        $result = 'High card';
+        $values = [];
+        $colors = [];
+        foreach ($cards as $card) {
+            $values[] = $card[0];
+            $colors[] = $card[1];
+        }
+        usort($values, function($a,$b) {
+            if(self::ORDER_VALUES[$a] === self::ORDER_VALUES[$b]) {
+                return 0;
+            }
+            return self::ORDER_VALUES[$a] < self::ORDER_VALUES[$b]? -1 : 1;
+        });
+        $countValues = array_count_values($values);
+        foreach ($countValues as $count) {
+            switch ($count) {
+                case 2:
+                    $combinaisons['One pair']++;
+                    if($combinaisons['One pair'] === 2) {
+                       $combinaisons['One pair'] = 0;
+                        $combinaisons['Two pairs'] = 1;
+
+                    }
+                    break;
+                case 3:
+                    if($combinaisons['One pair'] === 1) {
+                        $combinaisons['One pair'] = 0;
+                        $combinaisons['Full house']++;
+                    } else{
+                        $combinaisons['Three of a kind']++;
+                    }
+                    break;
+                case 4:
+                    $combinaisons['Two pairs'] = 0;
+                    $combinaisons['Four of a kind']++;
+                    break;
+            }
+        }
+        $countColors = array_count_values($colors);
+        if(in_array(count($colors),$countColors)) {
+            $combinaisons['Flush']++;
+        }
+        $countSuite = 0;
+        for($i = 0; $i < count($values) - 1; $i++) {
+            if(self::ORDER_VALUES[$values[$i+1]] - self::ORDER_VALUES[$values[$i]] === 1) {
+                $countSuite++;
+            }
+        }
+         if($countSuite === 4){
+             $combinaisons['Straight']++;
+             if($combinaisons['Flush'] === 1) {
+                 $combinaisons['Flush'] = 0;
+                 $combinaisons['Straight flush']++;
+             }
+         }
+         if($countSuite === 3 && (self::ORDER_VALUES[$values[count($values) - 1]] - self::ORDER_VALUES[$values[0]]) === 12) {
+             $combinaisons['Straight']++;
+             if($combinaisons['Flush'] === 1) {
+                 $combinaisons['Flush'] = 0;
+                 $combinaisons['Royal flush']++;
+             }
+         }
+        foreach ($combinaisons as $combinaisonName => $countCombinaison) {
+            if($countCombinaison !== 0) {
+                $result = $combinaisonName;
+            }
+        }
+        return $result;
     }
 }
--- PokerTest.php
+++ PokerTest.php
@@ -10,7 +10,17 @@ class PokerTest extends TestCase
     public function testRetrieveCombinaison()
     {
         $poker = new Poker();
-        $this->assertEquals('nothing',$poker->retrieveCombinaison([['A','H'],['2','C'],['4','S'],['8','D'],['10','S']]));
-        $this->assertEquals('nothing',$poker->retrieveCombinaison([['A','H'],['2','C'],['4','S'],['8','D'],['10','S']]));
+        $this->assertEquals('High card',$poker->retrieveCombinaison([['A','H'],['2','C'],['4','S'],['8','D'],['10','S']]));
+        $this->assertEquals('One pair',$poker->retrieveCombinaison([['A','H'],['A','C'],['4','S'],['8','D'],['10','S']]));
+        $this->assertEquals('Three of a kind',$poker->retrieveCombinaison([['A','H'],['A','C'],['A','S'],['8','D'],['10','S']]));
+        $this->assertEquals('Four of a kind',$poker->retrieveCombinaison([['A','H'],['A','C'],['A','S'],['A','D'],['10','S']]));
+        $this->assertEquals('Two pairs',$poker->retrieveCombinaison([['A','H'],['A','C'],['3','S'],['3','D'],['10','S']]));
+        $this->assertEquals('Full house',$poker->retrieveCombinaison([['A','H'],['A','C'],['3','S'],['3','D'],['3','S']]));
+        $this->assertEquals('Flush',$poker->retrieveCombinaison([['A','H'],['3','H'],['5','H'],['10','H'],['K','H']]));
+        $this->assertEquals('Straight',$poker->retrieveCombinaison([['2','H'],['3','S'],['A','H'],['5','H'],['4','H']]));
+        $this->assertEquals('Straight',$poker->retrieveCombinaison([['A','H'],['K','S'],['Q','H'],['J','H'],['10','H']]));
+        $this->assertEquals('Straight',$poker->retrieveCombinaison([['A','H'],['K','S'],['Q','H'],['J','H'],['10','H']]));
+        $this->assertEquals('Straight flush',$poker->retrieveCombinaison([['A','H'],['2','H'],['5','H'],['4','H'],['3','H']]));
+        $this->assertEquals('Royal flush',$poker->retrieveCombinaison([['A','H'],['J','H'],['K','H'],['Q','H'],['10','H']]));
     }
 }
